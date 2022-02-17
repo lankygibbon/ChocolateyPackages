5a773cc706f262ccbdfb58a5e07cc783e8ffc32e
lankygibbon <matthewjamesseymour@gmail.com>
2022-02-17 16:14:29 +0000

    first commit

---
+++ nandeck/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    PackageName  = "nandeck.portable"
+    File         = "$toolsPath\nandeck.zip"
+    Destination  = $toolsPath
+}
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item -force "$toolsPath\*.zip" -ea 0
